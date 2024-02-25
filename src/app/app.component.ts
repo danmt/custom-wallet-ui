4b386a380f34abd88fef30880e5b33d7bf667c69
Dan <danielarturomt@gmail.com>
2024-02-25 16:15:10 +0000

    feat: final solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgClass } from '@angular/common';
+import { Component, computed } from '@angular/core';
+import {
+  injectConnected,
+  injectPublicKey,
+  injectWallet,
+} from '@heavy-duty/wallet-adapter';
+import { HdObscureAddressPipe } from '@heavy-duty/wallet-adapter-cdk';
+import { WalletMultiButtonComponent } from './wallet-multi-button.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'semana-4-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <header class="p-8">
+      <h1 class="text-2xl text-center">Wallet Adapter Example (CDK)</h1>
+    </header>
+
+    <main class="max-w-[36rem] mx-auto border-2 border-black p-4">
+      <section>
+        <p>
+          Wallet:
+          {{ walletName() }}
+        </p>
+
+        <p>
+          Public Key:
+
+          @if (publicKey(); as publicKey) {
+            <span>
+              {{ publicKey.toBase58() | hdObscureAddress }}
+            </span>
+          } @else {
+            <span> None </span>
+          }
+        </p>
+
+        <p>
+          Status:
+          <span
+            [ngClass]="{
+              'text-red-600': !connected(),
+              'text-green-600': connected()
+            }"
+          >
+            {{ connected() ? 'Connected' : 'Disconnected' }}
+          </span>
+        </p>
+      </section>
+
+      <section>
+        <semana-4-wallet-multi-button></semana-4-wallet-multi-button>
+      </section>
+    </main>
+  `,
+  imports: [NgClass, HdObscureAddressPipe, WalletMultiButtonComponent],
 })
 export class AppComponent {
-  title = 'semana-4';
+  readonly wallet = injectWallet();
+  readonly connected = injectConnected();
+  readonly publicKey = injectPublicKey();
+  readonly walletName = computed(() => this.wallet()?.adapter.name ?? 'None');
 }
